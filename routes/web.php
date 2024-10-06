b887ea7e79b40601b4447bd2a3ee02d2ec918a21
naymenor <naymul.haque.89@gmail.com>
2024-10-06 13:40:12 +0600

    auth login

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login', function() {
+    return response()->json(['success' => false,'message' => 'Unauthorized'], 401);
+});
+
+Route::post('login', [ 'as' => 'login']);
+
